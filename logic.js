0a3d2dcad6f23d814bc0f51f0b81e3e941da2f09
uzzawalgupta <ujjawalgupta3000@gmail.com>
2025-03-09 12:40:28 +0530

    Create logic.js

---
+++ logic.js
@@ -0,0 +1,16 @@
+document.querySelector("#submit").disabled=true;
+document.querySelector("#task").onkeyup=()=>{
+        if (document.querySelector("#task").value!=="")
+                document.querySelector("#submit").disabled=false;
+        else
+                document.querySelector("#submit").disabled=true;
+}
+document.querySelector("form").onsubmit=()=>{
+        const task=document.querySelector("#task").value;
+        const li=document.createElement("li");
+        li.innerHTML=task;
+        document.querySelector("#tasks").append(li);
+        document.querySelector("#task").value="";
+        document.querySelector("#submit").disabled=true;
+        return false;
+}
